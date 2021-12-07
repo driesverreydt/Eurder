d933ef040fd03f91ad8eae989a4c1d92e084e2cf
Dries Verreydt <johndoe@example.com>
2021-12-07 11:39:50 +0100

    git commit -m "Story 1 added error logging to exceptions"

--- src/main/java/com/switchfully/projects/eurder/domain/user/User.java
+++ src/main/java/com/switchfully/projects/eurder/domain/user/User.java
@@ -1,7 +1,10 @@
 package com.switchfully.projects.eurder.domain.user;
 
+import com.switchfully.projects.eurder.api.ControllerExceptionHandler;
 import com.switchfully.projects.eurder.domain.exception.InvalidUserInformationException;
 import com.switchfully.projects.eurder.security.UserRole;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.UUID;
 
@@ -14,6 +17,7 @@ public class User {
     private final String password;
     private final PhoneNumber phoneNumber;
     private final UserRole userRole;
+    private final Logger logger = LoggerFactory.getLogger(User.class);
 
     public User(Name name, Address address, EmailAddress emailAddress, String password, PhoneNumber phoneNumber, UserRole userRole) {
         userInformationValidation(name, address, emailAddress, password, phoneNumber, userRole);
@@ -57,27 +61,35 @@ public class User {
     private void userInformationValidation(Name name, Address address, EmailAddress emailAddress, String password,
                                            PhoneNumber phoneNumber, UserRole userRole) {
         if (name == null) {
+            logger.info("At user creation name was null");
             throw new InvalidUserInformationException("A user requires a name");
         }
         if (name.getFirstName() == null) {
+            logger.info("At user creation first name was null");
             throw new InvalidUserInformationException("A user requires a first name");
         }
         if (name.getLastName() == null) {
+            logger.info("At user creation last name was null");
             throw new InvalidUserInformationException("A user requires a last name");
         }
         if (address == null) {
+            logger.info("At user creation address was null");
             throw new InvalidUserInformationException("A user requires an address");
         }
         if (emailAddress == null) {
+            logger.info("At user creation email address was null");
             throw new InvalidUserInformationException("A user requires an email address");
         }
         if (password == null) {
+            logger.info("At user creation password was null");
             throw new InvalidUserInformationException("A user requires a password");
         }
         if (phoneNumber == null) {
+            logger.info("At user creation phone number was null");
             throw new InvalidUserInformationException("A user requires a phone number");
         }
         if (userRole == null) {
+            logger.info("At user creation user role was null");
             throw new InvalidUserInformationException("A user requires a user role");
         }
     }
--- src/main/java/com/switchfully/projects/eurder/security/AuthorizationService.java
+++ src/main/java/com/switchfully/projects/eurder/security/AuthorizationService.java
@@ -4,6 +4,8 @@ import com.switchfully.projects.eurder.domain.exception.AuthorisationNotGrantedE
 import com.switchfully.projects.eurder.domain.user.EmailAddress;
 import com.switchfully.projects.eurder.domain.user.User;
 import com.switchfully.projects.eurder.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.Base64;
@@ -16,6 +18,7 @@ public class AuthorizationService {
 
     private final Map<EurderFeature, UserRole> featureAuthorizations;
     private final UserService userService;
+    private final Logger logger = LoggerFactory.getLogger(AuthorizationService.class);
 
     public AuthorizationService(UserService userService) {
         this.userService = userService;
@@ -25,6 +28,9 @@ public class AuthorizationService {
 
     public void authorize(EurderFeature eurderFeature, String authorization) {
         if (featureAuthorizations.get(eurderFeature).getAuthorizationLevel() > getAuthorizationLevel(authorization)) {
+            logger.error("At authorization a user with role " +
+                    UserRole.getNameForAuthorizationLevel(getAuthorizationLevel(authorization)).name().toLowerCase() +
+                    " tried to access feature " + eurderFeature.name().toLowerCase());
             throw new AuthorisationNotGrantedException("User with role " +
                     UserRole.getNameForAuthorizationLevel(getAuthorizationLevel(authorization)).name().toLowerCase() +
                     " does not have rights to feature " + eurderFeature.name().toLowerCase());
@@ -35,26 +41,30 @@ public class AuthorizationService {
         UserRole userRole = UserRole.GUEST;
         if (authorization != null) {
             User existingUser = getUser(authorization);
-            checkPassword(existingUser.getPassword(),authorization);
+            checkPassword(existingUser.getPassword(), authorization);
             userRole = existingUser.getUserRole();
         }
         return userRole.getAuthorizationLevel();
     }
 
-    private User getUser(String authorization){
+    private User getUser(String authorization) {
         String userEmailAddressString = parseAuthorizationUserName(authorization);
         EmailAddress userEmailAddress = EmailAddress.parseEmailAddressString(userEmailAddressString);
         Collection<User> usersByEmail = userService.getUsersByEmail(userEmailAddress);
-        if(usersByEmail.isEmpty()){
+        if (usersByEmail.isEmpty()) {
+            logger.error("At authorization a user with email " + userEmailAddress.getFullEmailAddress() +
+                    " could not be verified as no user with this email could be found");
             throw new AuthorisationNotGrantedException("No user found with email " + userEmailAddress.getFullEmailAddress());
         }
         return usersByEmail.iterator().next();
     }
 
-    private void checkPassword(String expectedPassword, String authorization){
+    private void checkPassword(String expectedPassword, String authorization) {
         String userPassword = parseAuthorizationPassword(authorization);
         String userEmailAddressString = parseAuthorizationUserName(authorization);
-        if(!expectedPassword.equals(userPassword)){
+        if (!expectedPassword.equals(userPassword)) {
+            logger.error("At authorization the password " + userPassword +
+                    " did not match the password for user with email " + userEmailAddressString);
             throw new AuthorisationNotGrantedException("Password " + userPassword +
                     " does not match the password for user with email " + userEmailAddressString);
         }
@@ -67,7 +77,7 @@ public class AuthorizationService {
 
     private String parseAuthorizationPassword(String authorization) {
         String decodedUsernameAndPassword = new String(Base64.getDecoder().decode(authorization.substring("Basic ".length())));
-        return decodedUsernameAndPassword.substring(decodedUsernameAndPassword.indexOf(":")+1);
+        return decodedUsernameAndPassword.substring(decodedUsernameAndPassword.indexOf(":") + 1);
     }
 
 }
